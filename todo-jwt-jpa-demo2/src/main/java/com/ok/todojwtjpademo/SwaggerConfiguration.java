677ca5c7f491c59401610f07d0fb6384f225b659
oguz <oguzkayaya@gmail.com>
2020-08-19 14:42:13 +0300

    springfox swagger2 added and enabled

---
+++ todo-jwt-jpa-demo2/src/main/java/com/ok/todojwtjpademo/SwaggerConfiguration.java
@@ -0,0 +1,13 @@
+package com.ok.todojwtjpademo;
+
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableSwagger2
+@Configuration
+public class SwaggerConfiguration {
+
+}
+
+
+
